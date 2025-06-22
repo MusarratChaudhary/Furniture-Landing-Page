887ffe1df4bb99537106ae67fb6a7475803bafc7
Musarrat Chaudhary <musarratchaudhary797@gmail.com>
2025-06-22 20:53:33 +0500

    Added TypeScript functionality to button

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+const button = document.querySelector('.main-button');
+if (button) {
+    button.addEventListener('click', () => {
+        alert("Thank you for visiting Modern Furniture!");
+    });
+}
---
+++ script.ts
@@ -0,0 +1,10 @@
+// const button = document.querySelector('.main-button') as HTMLButtonElement;
+
+// if (button) {
+//   button.addEventListener('click', () => {
+//     alert("Thank you for visiting Modern Furniture!");
+//   });
+// }
+
+
+
